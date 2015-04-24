971d7e886a70ef35fea08ef25ec4e834fb5d7e63
Adam de Jong <dejonga@onid.oregonstate.edu>
2015-04-24 00:58:08 -0600

    Added ability to grab gists and display them on html page

--- functions.js
+++ functions.js
@@ -1,10 +1,16 @@
 
 
+var originalGistList = [];
+var xmlhttp;
 
+function coolStuff(url) 
+{
+  originalGistList = loadXMLDoc(url);
+}
 
 function loadXMLDoc(url) //I borrowed the bulk of this code from the w3schools website to help me get the xmlhttp object set up correctly
 {
-var xmlhttp;
+
 if (window.XMLHttpRequest)
   {// code for IE7+, Firefox, Chrome, Opera, Safari
   xmlhttp=new XMLHttpRequest();
@@ -14,39 +20,48 @@ else
   xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
   }
 
-xmlhttp.onreadystatechange=function()
-  {
+xmlhttp.onreadystatechange=function() 
+ {
   if (xmlhttp.readyState==4 && xmlhttp.status==200)
     {
-    document.getElementById('result').innerHTML=xmlhttp.responseText;
+      var obj = JSON.parse(xmlhttp.responseText);
+
+     document.getElementById('result').innerHTML=obj[1].description;
+     //document.getElementById('result').innerHTML=xmlhttp.responseText;
     }
-  }
+ }
+
 xmlhttp.open("GET",url,true);
 xmlhttp.send();
-/*
-if (!xmlhttp)
-  alert("Some kind of failure!");
-else
-    return xmlhttp;
-*/
+
+
+}
+
+
+function printStuff()
+{
 
 }
 
 
 /*
+if (!xmlhttp)
+  alert("Some kind of failure!");
+else
+    return xmlhttp;
 
 xmlhttp.onreadystatechange=function()
   {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+  
+if (xmlhttp.readyState==4 && xmlhttp.status==200)
     {
-    document.getElementById('A1').innerHTML=xmlhttp.status;
-    document.getElementById('A2').innerHTML=xmlhttp.statusText;
-    document.getElementById('A3').innerHTML=xmlhttp.responseText;
+    document.getElementById('result').innerHTML=xmlhttp.responseText;
     }
-  }
-xmlhttp.open("GET",url,true);
-xmlhttp.send();
 
-
-
+      xmlhttp.open("GET",url,true);
+  xmlhttp.send();
+}
 */
+
+
+
