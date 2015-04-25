8196af75f4020f7d4f77eac07bc7af2de31929b5
Adam de Jong <dejonga@onid.oregonstate.edu>
2015-04-24 22:31:50 -0600

    Parse json result, add html styling to results, print results on page.  Checkbox functionality added.

--- functions.js
+++ functions.js
@@ -1,7 +1,11 @@
 
 
-var originalGistList = [];
+var favList = [];
 var xmlhttp;
+var x;
+var temp = "";
+
+var obj;
 
 function coolStuff(url) 
 {
@@ -24,22 +28,57 @@ xmlhttp.onreadystatechange=function()
  {
   if (xmlhttp.readyState==4 && xmlhttp.status==200)
     {
-      var obj = JSON.parse(xmlhttp.responseText);
+      obj = JSON.parse(xmlhttp.responseText);
 
-     document.getElementById('result').innerHTML=obj[1].description;
-     //document.getElementById('result').innerHTML=xmlhttp.responseText;
+      for (x = 0; x < obj.length; x++)
+      {
+        
+        temp = temp + '<li>' + '<input type="checkbox" name="rList" value="' + x + '" onclick="favTog(this.checked, this.value)">' + obj[x].description + '<br>' + '<a href="' + obj[x].html_url + '">' + obj[x].html_url + '</a>' + '</li>';
+        
+      }
+      temp = '<ul style="list-style-type:none">' + temp + '</ul>';
+     document.getElementById('result').innerHTML = temp;
     }
  }
 
 xmlhttp.open("GET",url,true);
 xmlhttp.send();
 
-
 }
 
 
-function printStuff()
+
+function favTog(stat,num)
 {
+
+  var tempList = [];
+  var temp2 = "";
+
+  document.getElementById('favs').innerHTML = "";
+
+  if (stat == true)
+  {
+    favList[favList.length] = obj[num];
+  }
+  else
+  {
+    for (x = 0; x < favList.length; x++)
+    {
+      if ( obj[num].id != favList[x].id )
+      {
+        tempList[tempList.length] = favList[x];
+      }
+    }
+    favList = tempList;
+  }
+
+  for (x = 0; x < favList.length; x++)
+  {
+    temp2 = temp2 + '<li>' + '<input type="checkbox" name="fList" value="' + x + '" onclick="">' + favList[x].description + '<br>' + '<a href="' + favList[x].html_url + '">' + favList[x].html_url + '</a>' + '</li>';
+  }
+ 
+  temp2 = '<ul style="list-style-type:none">' + temp2 + '</ul>';
+  document.getElementById('favs').innerHTML = temp2;
 
 }
 
