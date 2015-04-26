d05a257db3986c54f536a6671b7307bb8628e62f
Adam de Jong <dejonga@onid.oregonstate.edu>
2015-04-25 22:42:53 -0600

    Added CSS, working add/remove checkbox functionality

--- functions.js
+++ functions.js
@@ -3,13 +3,20 @@
 var favList = [];
 var xmlhttp;
 var x;
+var y;
 var temp = "";
-
 var obj;
+
+window.onload = function()
+{
+  document.getElementById('favs').innerHTML = localStorage.getItem("favorites");
+}
+
+
 
 function coolStuff(url) 
 {
-  originalGistList = loadXMLDoc(url);
+  loadXMLDoc(url);
 }
 
 function loadXMLDoc(url) //I borrowed the bulk of this code from the w3schools website to help me get the xmlhttp object set up correctly
@@ -33,7 +40,7 @@ xmlhttp.onreadystatechange=function()
       for (x = 0; x < obj.length; x++)
       {
         
-        temp = temp + '<li>' + '<input type="checkbox" name="rList" value="' + x + '" onclick="favTog(this.checked, this.value)">' + obj[x].description + '<br>' + '<a href="' + obj[x].html_url + '">' + obj[x].html_url + '</a>' + '</li>';
+        temp = temp + '<li>' + '<input type="checkbox" name="rList" value="' + obj[x].id + '" onclick="favTog(this.checked, this.value)">' + obj[x].description + '<br>' + '<a href="' + obj[x].html_url + '">' + obj[x].html_url + '</a>' + '</li>';
         
       }
       temp = '<ul style="list-style-type:none">' + temp + '</ul>';
@@ -47,41 +54,127 @@ xmlhttp.send();
 }
 
 
-
 function favTog(stat,num)
 {
 
   var tempList = [];
   var temp2 = "";
+  var bool = false;
+  var temp3 = "";
 
   document.getElementById('favs').innerHTML = "";
-
+  //This section adds an item to the favorite list if checked, and removes it if unchecked
   if (stat == true)
   {
-    favList[favList.length] = obj[num];
+    for (x = 0; x < obj.length; x++)
+    {
+      if (obj[x].id == num)
+      {
+        favList[favList.length] = obj[x];
+      }
+    }
+    
   }
   else
   {
     for (x = 0; x < favList.length; x++)
     {
-      if ( obj[num].id != favList[x].id )
+      if ( num != favList[x].id )
       {
         tempList[tempList.length] = favList[x];
       }
     }
     favList = tempList;
   }
-
+  //This section prints the completed fav list
   for (x = 0; x < favList.length; x++)
   {
-    temp2 = temp2 + '<li>' + '<input type="checkbox" name="fList" value="' + x + '" onclick="">' + favList[x].description + '<br>' + '<a href="' + favList[x].html_url + '">' + favList[x].html_url + '</a>' + '</li>';
+    temp2 = temp2 + '<li>' + '<input type="checkbox" name="fList" value="' + favList[x].id + '" onclick="favRemove(this.checked, this.value)">' + favList[x].description + '<br>' + '<a href="' + favList[x].html_url + '">' + favList[x].html_url + '</a>' + '</li>';
   }
  
   temp2 = '<ul style="list-style-type:none">' + temp2 + '</ul>';
+  document.getElementById('favs').innerHTML = "";
   document.getElementById('favs').innerHTML = temp2;
+  localStorage.setItem("favorites", temp2);
+
+  //This section reprints the results list so that it does not include the favlist items
+  for (x = 0; x < obj.length; x++)
+  {
+    bool = false;
+    for (y=0; y < favList.length; y++)
+    {
+      if (obj[x].id == favList[y].id)
+      {
+        bool = true;
+      }
+
+    }
+
+    if (bool == false)
+    {
+     temp3 = temp3 + '<li>' + '<input type="checkbox" name="rList" value="' + obj[x].id + '" onclick="favTog(this.checked, this.value)">' + obj[x].description + '<br>' + '<a href="' + obj[x].html_url + '">' + obj[x].html_url + '</a>' + '</li>';
+    }
+  }
+  
+  temp3 = '<ul style="list-style-type:none">' + temp3 + '</ul>';
+  document.getElementById('result').innerHTML = "";
+  document.getElementById('result').innerHTML = temp3;
+
 
 }
 
+function favRemove(stat,num)
+{
+
+  var tempList2 = [];
+  var temp4 = "";
+  var temp5 = "";
+
+  //This loop removes the checked fav item from the fav list
+  for (x = 0; x < favList.length; x++)
+  {
+    if (favList[x].id != num)
+    {
+      tempList2[tempList2.length] = favList[x];
+    }
+  }
+  favList = tempList2;
+
+  //This section prints the updated fav list
+  for (x = 0; x < favList.length; x++)
+  {
+    temp4 = temp4 + '<li>' + '<input type="checkbox" name="fList" value="' + favList[x].id + '" onclick="favRemove(this.checked, this.value)">' + favList[x].description + '<br>' + '<a href="' + favList[x].html_url + '">' + favList[x].html_url + '</a>' + '</li>';
+  }
+ 
+  temp4 = '<ul style="list-style-type:none">' + temp4 + '</ul>';
+  document.getElementById('favs').innerHTML = "";
+  document.getElementById('favs').innerHTML = temp4;
+  localStorage.setItem("favorites", temp4);
+
+  //This section reprints the results list so that it does not include the favlist items
+  for (x = 0; x < obj.length; x++)
+  {
+    bool = false;
+    for (y=0; y < favList.length; y++)
+    {
+      if (obj[x].id == favList[y].id)
+      {
+        bool = true;
+      }
+
+    }
+
+    if (bool == false)
+    {
+     temp5 = temp5 + '<li>' + '<input type="checkbox" name="rList" value="' + obj[x].id + '" onclick="favTog(this.checked, this.value)">' + obj[x].description + '<br>' + '<a href="' + obj[x].html_url + '">' + obj[x].html_url + '</a>' + '</li>';
+    }
+  }
+  
+  temp5 = '<ul style="list-style-type:none">' + temp5 + '</ul>';
+  document.getElementById('result').innerHTML = "";
+  document.getElementById('result').innerHTML = temp5;
+
+}
 
 /*
 if (!xmlhttp)
